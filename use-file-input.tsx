8ef21d1c55afe4532aa7aa68a9001f422d4755e0
Matias Perez <43894343+matiasperz@users.noreply.github.com>
2024-11-01 12:26:59 -0300

    Create use-file-input.tsx

---
+++ use-file-input.tsx
@@ -0,0 +1,50 @@
+import { useCallback, useId, useMemo } from "react"
+
+interface FileInputProps {
+  onUpload: (file: File) => Promise<any> | void
+  inputProps?: React.InputHTMLAttributes<HTMLInputElement>
+}
+
+interface FileInputReturn {
+  handleFileChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  handleClick: () => void
+  input: JSX.Element
+}
+
+export const useFileInput = ({ onUpload, inputProps = {} }: FileInputProps): FileInputReturn => {
+  const id = useId()
+
+  const handleClick = useCallback(() => {
+    const fileInput = document.getElementById(`${id}-hidden-file-input`) as HTMLInputElement
+    fileInput?.click()
+  }, [id])
+
+  const handleFileChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const files = event.target.files
+      if (files && files.length > 0) {
+        Array.from(files).forEach((file) => {
+          onUpload(file)
+        })
+      }
+    },
+    [onUpload]
+  )
+
+  return useMemo(
+    () => ({
+      handleFileChange,
+      handleClick,
+      input: (
+        <input
+          type="file"
+          id={`${id}-hidden-file-input`}
+          style={{ width: 0, height: 0, opacity: 0, position: "absolute", pointerEvents: "none" }}
+          onChange={handleFileChange}
+          {...inputProps}
+        />
+      ),
+    }),
+    [handleClick, handleFileChange, id, inputProps]
+  )
+}
